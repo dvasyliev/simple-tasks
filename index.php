af9488d1341c8bcc9dd1d09e47cf9629ad5c2ac3
dvasyliev <dvasyliev.dev@gmail.com>
2017-07-27 12:25:09 +0300

    Refactored index file. Created and registered Classes of Loader, Db

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
 ini_set( 'display_errors', 1 );
+
+// Подключаем файл конфигурации
 require_once 'config.php';
+
+// Подключаем файл стартового загрузчика
 require_once 'app/bootstrap.php';
+
+// Создаем экземпляр класса регистратора
+$registry = new Registry();
+
+// Создаем экземпляр класса загрузчика и регистрируем его
+$loader = new Loader( $registry );
+$registry->set( 'load', $loader );
+
+// Создаем экземпляр класса базы данных и регистрируем его
+$db = new DB( DB_DRIVER, DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT );
+$registry->set( 'db', $db );
+
+// Создаем екземпляр класса маршрутизатора и запускаем его
+$router = new Router();
+$router->start( $registry );
