4795494d2e585523dbd78527f4adc1acb1785fd3
dvasyliev <dvasyliev.dev@gmail.com>
2017-07-27 12:06:32 +0300

    Added configuration for database. Renamed path constants

--- config.php
+++ config.php
@@ -4,10 +4,18 @@
  */
 
 // Defining paths
-define( 'DIR_ROOT', __DIR__ );
-define( 'DIR_CORE', ROOT . '/app/core');
-define( 'DIR_MODEL', ROOT . '/app/model');
-define( 'DIR_CONTROLLER', ROOT . '/app/controller');
-define( 'DIR_VIEW', ROOT . '/app/view');
-define( 'DIR_TEMPLATE', VIEW . '/template');
-define( 'DIR_CONFIG', ROOT . '/config');
+define( 'DIR_ROOT',         __DIR__ );
+define( 'DIR_CORE',         DIR_ROOT . '/app/core');
+define( 'DIR_MODEL',        DIR_ROOT . '/app/model');
+define( 'DIR_CONTROLLER',   DIR_ROOT . '/app/controller');
+define( 'DIR_VIEW',         DIR_ROOT . '/app/view');
+define( 'DIR_TEMPLATE',     DIR_VIEW . '/template');
+define( 'DIR_CONFIG',       DIR_ROOT . '/config');
+
+// Database configuration
+define( 'DB_DRIVER',        'mpdo' );
+define( 'DB_HOST',          'localhost' );
+define( 'DB_USER',          'root' );
+define( 'DB_PASSWORD',      '' );
+define( 'DB_DATABASE',      'simple_tasks' );
+define( 'DB_PORT',          '3306' );
