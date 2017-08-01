c24c24248fdd929fb57f5c2329388451db976306
dvasyliev <dvasyliev.dev@gmail.com>
2017-08-01 14:36:46 +0300

    Created header and footer templates

--- app/view/template/common/footer.php
+++ app/view/template/common/footer.php
@@ -1,4 +1,5 @@
-    <script type="text/javascript" src="app/view/node_modules/jquery/dist/jquery.min.js"></script>
-    <script type="text/javascript" src="app/view/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/app/view/node_modules/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="/app/view/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/app/view/src/js/main.js"></script>
 </body>
 </html>
--- app/view/template/common/header.php
+++ app/view/template/common/header.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="app/view/node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="app/view/node_modules/bootstrap/dist/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="/app/view/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/app/view/node_modules/bootstrap/dist/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="/app/view/src/css/main.css">
 </head>
 <body>
