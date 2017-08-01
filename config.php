df466922a211c2a24647d6783cb76aff1b6b06ce
dvasyliev <dvasyliev.dev@gmail.com>
2017-08-01 14:35:03 +0300

    Added configuration to config.php

--- config.php
+++ config.php
@@ -2,15 +2,17 @@
 /*
  * Configurations for project
  */
-
 // Defining paths
 define( 'DIR_ROOT',         __DIR__ );
-define( 'DIR_CORE',         DIR_ROOT . '/app/core');
-define( 'DIR_MODEL',        DIR_ROOT . '/app/model');
-define( 'DIR_CONTROLLER',   DIR_ROOT . '/app/controller');
-define( 'DIR_VIEW',         DIR_ROOT . '/app/view');
-define( 'DIR_TEMPLATE',     DIR_VIEW . '/template');
-define( 'DIR_CONFIG',       DIR_ROOT . '/config');
+define( 'DIR_CORE',         DIR_ROOT . '/app/core' );
+define( 'DIR_MODEL',        DIR_ROOT . '/app/model' );
+define( 'DIR_CONTROLLER',   DIR_ROOT . '/app/controller' );
+define( 'DIR_VIEW',         DIR_ROOT . '/app/view' );
+define( 'DIR_TEMPLATE',     DIR_VIEW . '/template' );
+define( 'DIR_CONFIG',       DIR_ROOT . '/config' );
+define( 'DIR_IMAGE',        DIR_ROOT . '/uploads' );
+
+define( 'BASE_URI' ,        'http://simple-tasks/');
 
 // Database configuration
 define( 'DB_DRIVER',        'mpdo' );
