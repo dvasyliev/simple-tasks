3690ff8ed28583a1c3a9a77e621c1a1f56737a0b
dvasyliev <dvasyliev.dev@gmail.com>
2017-08-02 19:21:32 +0300

    Created header with login/logout buttons

--- app/view/template/common/header.php
+++ app/view/template/common/header.php
@@ -10,3 +10,16 @@
     <link rel="stylesheet" href="/app/view/src/css/main.css">
 </head>
 <body>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">Simple Tasks</a>
+                <?php if( !$is_logged ): ?>
+                    <a href="/admin/login" class="navbar-btn btn btn-success">Войти как администратор</a>
+                <?php else: ?>
+                    <a href="/admin/logout" class="navbar-btn btn btn-success">Выйти</a>
+                    <span class="navbar-text">Вы вошли, как <b><?= $login ?></b></span>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
